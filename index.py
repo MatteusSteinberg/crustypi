c36fe291fb51c7ff50db37a8f35d43df815a33d7
Emil <emilskovmand@gmail.com>
2023-08-08 11:19:04 +0200

    script sensors

--- index.py
+++ index.py
@@ -1,2 +1,15 @@
+from sense_hat import SenseHat
+
 while (True):
-    print("Sejt!")
+    sense = SenseHat()
+
+    # Percentage of relative humidity
+    humidity = sense.get_humidity()
+    # Current temperature in degrees Celsius
+    temp = sense.get_temperature()
+    # Current pressure in Millibars
+    pressure = sense.get_pressure()
+
+    print("Humidity: " + humidity + "%")
+    print("Temperacture: " + temp + "°C")
+    print("Pressure: " + pressure + " mb")
