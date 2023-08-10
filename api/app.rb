33e034de2534a64cd5e2c131d2c055956f8bc020
Matteus Steinberg <matteus@stnbrg.dk>
2023-08-10 08:55:19 +0200

    works?

--- api/app.rb
+++ api/app.rb
@@ -1,73 +1,55 @@
 require 'sinatra'
-require 'sinatra-websocket'
+require 'sinatra/base'
 
 require 'mongoid'
 require 'json'
 
 Mongoid.load!(File.join(File.dirname(__FILE__), 'config', 'mongoid.yml'))
 
-class Measures
-  include Mongoid::Document
+class MyApp < Sinatra::Base
+  class Measures
+    include Mongoid::Document
 
-  field :temperature, type: Integer
-  field :humidity, type: Integer
-  field :pressure, type: Integer
-  field :timestamp, type: Date
+    field :temperature, type: Integer
+    field :humidity, type: Integer
+    field :pressure, type: Integer
+    field :timestamp, type: Date
 
-end
+  end
 
-before do
-  content_type :json
+  before do
+    content_type :json
 
-  request.body.rewind
-  @request_payload = JSON.parse(request.body.read, symbolize_names: true)
-end
+    request.body.rewind
+    @request_payload = JSON.parse(request.body.read, symbolize_names: true)
+  end
 
-get '/measures' do
-  if params
-    page = params[:page].to_i || 1
-    per_page = params[:per_page].to_i || 10
-    measures = Measures.skip((page - 1) * per_page).limit(per_page)
-    total_pages = (Measures.count / per_page.to_f).ceil
-    {
-      measures: measures,
-      page: page,
-      per_page: per_page,
-      total_pages: total_pages
-    }.to_json
-  else
-    Measures.all.to_json
-  end 
-end
+  get '/measures' do
+    if params
+      page = params[:page].to_i || 1
+      per_page = params[:per_page].to_i || 10
+      measures = Measures.skip((page - 1) * per_page).limit(per_page)
+      total_pages = (Measures.count / per_page.to_f).ceil
+      {
+        measures: measures,
+        page: page,
+        per_page: per_page,
+        total_pages: total_pages
+      }.to_json
+    else
+      Measures.all.to_json
+    end 
+  end
 
-get '/' do
-  if !request.websocket?
-    erb :index
-  else
-    request.websocket do |ws|
-      ws.onopen do
-        ws.send("Hello World!")
-        settings.sockets << ws
-      end
-      ws.onmessage do |msg|
-        EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
-      end
-      ws.onclose do
-        warn("websocket closed")
-        settings.sockets.delete(ws)
-      end
+  post '/measures' do
+    @request_payload.each do |data|
+      Measures.create!(data)
     end
+    status 201
   end
-end
 
-post '/measures' do
-  @request_payload.each do |data|
-    Measures.create!(data)
+  get '/measures/:measure' do |measure|
+    measures = Measures.find(measure)
+    measures.to_json
   end
-  status 201
-end
-
-get '/measures/:measure' do |measure|
-  measures = Measures.find(measure)
-  measures.to_json
 end
