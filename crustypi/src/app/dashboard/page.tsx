9e99fec8b994f2b99159e88a97cabb79068a299f
Matteus Steinberg <matteus@stnbrg.dk>
2023-08-11 12:11:35 +0200

    vercel fix?

--- crustypi/src/app/dashboard/page.tsx
+++ crustypi/src/app/dashboard/page.tsx
@@ -25,7 +25,7 @@ ChartJS.register(
   Legend
 );
 
-export const options = {
+const options = {
   responsive: true,
   plugins: {
     legend: {
@@ -36,7 +36,7 @@ export const options = {
 
 const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
-export const data = {
+const data = {
   labels,
   datasets: [
     {
