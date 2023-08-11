0aaf257463af590286e33ea42d189150fbea8490
Matteus Steinberg <matteus@stnbrg.dk>
2023-08-11 12:08:40 +0200

    poop

---
+++ crustypi/src/app/dashboard/page.tsx
@@ -0,0 +1,76 @@
+"use client"
+
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar, Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    }
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => Math.floor(Math.random() * 1000)),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => Math.floor(Math.random() * 1000)),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export default function Home() {
+
+  return (
+    <main>
+      <div className="container">
+        <div className='header'>
+          <h1>Crusty Pi</h1>
+        </div>
+        <div className='content'>
+          <div className='charts'>
+            <Bar options={options} data={data} />
+            <Line options={options} data={data} />
+            <Bar options={options} data={data} />
+            <Bar options={options} data={data} />
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
