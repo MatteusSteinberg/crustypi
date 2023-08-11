9b4df4ec9d2a87b74f1c215f216d9b8db6528da1
Emil <emilskovmand@gmail.com>
2023-08-11 12:39:23 +0200

    add sleep

--- arduino.py
+++ arduino.py
@@ -1,4 +1,5 @@
 from pyfirmata import Arduino
+import time
 
 portname = "/dev/ttyACM1"
 
@@ -9,4 +10,5 @@ def arduino_board():
     print("Arduino connection made!")
 
     while True:
+        time.sleep(10)
         print("Arduino loop")
