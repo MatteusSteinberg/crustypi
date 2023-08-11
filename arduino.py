9b8080fc43ede8dd8fef23189f44252c96b1b7e5
Emil <emilskovmand@gmail.com>
2023-08-11 12:42:32 +0200

    print pin read

--- arduino.py
+++ arduino.py
@@ -13,5 +13,8 @@ def arduino_board():
         analog_0 = board.get_pin('a:0:i')
         digital_7 = board.get_pin('d:7:i')
 
+        print(analog_0.read())
+        print(digital_7.read())
+
         time.sleep(10)
         print("Arduino loop")
