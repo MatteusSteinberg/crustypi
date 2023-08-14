faf5a41cdca2b07afb758ec6a443f7726dd327f1
Emil <emilskovmand@gmail.com>
2023-08-14 08:32:18 +0200

    hehe

--- arduino.py
+++ arduino.py
@@ -1,5 +1,6 @@
-from pyfirmata import Arduino
+from pyfirmata import Arduino, util
 import time
+from datetime import datetime
 
 portname = "/dev/ttyACM1"
 
@@ -12,10 +13,16 @@ def arduino_board():
     analog_0 = board.get_pin('a:0:i')
     digital_7 = board.get_pin('d:7:i')
 
-    while True:
+    it = util.Iterator(board)
+    it.start()
+    board.analog[0].enable_reporting()
 
-        print(analog_0.read())
-        print(digital_7.read())
+    while True:
+        now = datetime.now()
+        dt_string = now.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+        print(dt_string)
+
+        print(f"board.analog[0].read(): {board.analog[0].read()}")
 
         time.sleep(10)
         print("Arduino loop")
