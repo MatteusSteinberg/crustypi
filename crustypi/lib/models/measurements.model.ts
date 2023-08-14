a83f64665dab8fafc61accc4e24f673b59ca9606
Emil <emilskovmand@gmail.com>
2023-08-14 09:28:30 +0200

    add indexes

--- crustypi/lib/models/measurements.model.ts
+++ crustypi/lib/models/measurements.model.ts
@@ -23,5 +23,15 @@ const measurementSchema = new Schema({
 
 measurementSchema.index({ timestamp: -1 });
 
+measurementSchema.index({ timestamp: -1, temperature: 1 })
+
+measurementSchema.index({ timestamp: -1, humidity: 1 })
+
+measurementSchema.index({ timestamp: -1, pressure: 1 })
+
+measurementSchema.index({ timestamp: -1, detectedMotion: 1 })
+
+measurementSchema.index({ timestamp: -1, gas: 1 }, { partialFilterExpression: {'email': { $exists: true } } })
+
 const Measurement = model('Measurement', measurementSchema);
 export default Measurement;
