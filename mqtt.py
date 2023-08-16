12a901e8d67cd4c86324286b545f13434475e9e9
Emil <emilskovmand@gmail.com>
2023-08-16 08:21:52 +0200

    print creds

--- mqtt.py
+++ mqtt.py
@@ -23,6 +23,8 @@ class mqtt_client():
 
       self.client.tls_set(tls_version=ssl.PROTOCOL_TLS)  # Set the TLS version
 
+      print(self.username, self.password)
+
       self.client.username_pw_set(self.username, self.password)
 
       self.client.connect(self.broker_address, self.port, 60)
